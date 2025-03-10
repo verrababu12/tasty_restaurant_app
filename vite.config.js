ca4f5348a5882fa81127ac54bbc22b4309d9e86c
veeru <veeru@gmail.com>
2025-03-10 18:11:09 +0530

    finally

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Important for Vercel deployment
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    historyApiFallback: true,
-  },
 });
