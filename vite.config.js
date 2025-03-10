3a428e5c7d18f0ffd96b700cd6ec4ed844f9641b
veeru <veeru@gmail.com>
2025-03-10 17:35:42 +0530

    noyes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Fixes routing issue
+  base: "/", // Important for Vercel deployment
   build: {
     outDir: "dist",
   },
